d779c72bd7f1455d9d41ec11a312355c133ebbe5
lurendie <549700459@qq.com>
2024-04-04 23:03:19 +0800

    🐞 fix(rabtis/log): rbatis日志调整
    rbatis日志调整DEBUG,调整初始化方式,去除多余引用mod

--- src/app.rs
+++ src/app.rs
@@ -1,42 +1,52 @@
-use actix_web::http::header::{AUTHORIZATION, ACCEPT, ACCESS_CONTROL_ALLOW_ORIGIN, CONTENT_TYPE, ACCESS_CONTROL_REQUEST_HEADERS};
-use actix_cors::Cors;
-use actix_web::{web, App, HttpServer};
 use crate::config::Config;
-use crate::controller::{index_controller,blog_controller,archive_controller,moment_controller,about_controller,friend_controller};
 use crate::controller::admin::index_controller as adminIndexController;
+use crate::controller::{
+    about_controller, archive_controller, blog_controller, friend_controller, index_controller,
+    moment_controller,
+};
+use actix_cors::Cors;
+use actix_web::http::header::{
+    ACCEPT, ACCESS_CONTROL_ALLOW_ORIGIN, ACCESS_CONTROL_REQUEST_HEADERS, AUTHORIZATION,
+    CONTENT_TYPE,
+};
+use actix_web::{web, App, HttpServer};
 
-//app run 
-pub async fn run(conf : &Config) ->std::io::Result<()>{
+//app run
+pub async fn run(conf: &Config) -> std::io::Result<()> {
     HttpServer::new(|| {
-    // 配置 CORS
-    let cors = Cors::default()
-    //.allowed_origin(&conf.server.front_adderss) // 只允许 example.com 域进行跨域请求,暂时不开启
-    .allow_any_origin()
-    .allowed_methods(vec!["GET", "POST","PUT","DELETE"]) // 允许的 HTTP 方法
-    .allowed_headers(vec![AUTHORIZATION, ACCEPT,ACCESS_CONTROL_ALLOW_ORIGIN,CONTENT_TYPE,ACCESS_CONTROL_REQUEST_HEADERS])
-    .max_age(3600); // 设置 preflight 缓存的最大时间
-    App::new()
-    //跨域请求
-    .wrap(cors)//允许跨域请求
-    //service层
-    .service(index_controller::site)
-    .service(blog_controller::blogs)
-    .service(blog_controller::category)
-    .service(blog_controller::blog)
-    .service(blog_controller::tag)
-    .service(archive_controller::archives)  
-    .service(moment_controller::moments)  
-    .service(about_controller::about)
-    .service(friend_controller::get_friend)
-    
-    .service(web::scope("/admin/")//admin
-    .default_service(web::to(adminIndexController::default)))
-    .default_service(web::to(index_controller::default))
+        // 配置 CORS
+        let cors = Cors::default()
+            //.allowed_origin(&conf.server.front_adderss.as_str().clone()) // 只允许 example.com 域进行跨域请求,暂时不开启
+            .allow_any_origin()
+            .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"]) // 允许的 HTTP 方法
+            .allowed_headers(vec![
+                AUTHORIZATION,
+                ACCEPT,
+                ACCESS_CONTROL_ALLOW_ORIGIN,
+                CONTENT_TYPE,
+                ACCESS_CONTROL_REQUEST_HEADERS,
+            ])
+            .max_age(3600); // 设置 preflight 缓存的最大时间
+        App::new()
+            //跨域请求
+            .wrap(cors) //允许跨域请求
+            //service层
+            .service(index_controller::site)
+            .service(blog_controller::blogs)
+            .service(blog_controller::category)
+            .service(blog_controller::blog)
+            .service(blog_controller::tag)
+            .service(archive_controller::archives)
+            .service(moment_controller::moments)
+            .service(about_controller::about)
+            .service(friend_controller::get_friend)
+            .service(
+                web::scope("/admin/") //admin
+                    .default_service(web::to(adminIndexController::default)),
+            )
+            .default_service(web::to(index_controller::default))
     })
-    .bind(format!("{}:{}",conf.server.address,conf.server.port))?
+    .bind(format!("{}:{}", conf.server.address, conf.server.port))?
     .run()
     .await
 }
-
-
-
--- src/log4rs.rs
+++ src/log4rs.rs
@@ -1,21 +1,20 @@
 use chrono::Local;
+#[derive(Default)]
 pub struct Log4rs;
-
-impl Log4rs{
-    pub fn new()->Log4rs{
-        Log4rs{}.init()
+impl Log4rs {
+    pub fn new() -> Log4rs {
+        Log4rs::default().init()
     }
-    fn init (self)->Self{
-        let _ =log4rs::init_file("./conf/log4rs.yaml", Default::default()).unwrap();
+    fn init(self) -> Self {
+        let _ = log4rs::init_file("./conf/log4rs.yaml", Default::default()).unwrap();
         log::info!("开始加载项目, 时间为:[{}]...", get_date_time());
-         //修改日志等级ERROR 非ERROR日志不记录
-         log::set_max_level(log::LevelFilter::Error.to_level().unwrap().to_level_filter());
+        //修改日志等级ERROR 非ERROR日志不记录
+        //log::set_max_level(log::LevelFilter::Error.to_level().unwrap().to_level_filter());
         self
     }
 }
 
 pub const FMT_Y_M_D_H_M_S: &str = "%Y-%m-%d %H:%M:%S";
-
 
 pub fn get_date_time() -> String {
     let date_time = Local::now();
